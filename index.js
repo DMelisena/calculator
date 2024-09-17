2fc3e53f445f57a4a7851cad795cbd6e406d75fb
DMelisena <demonlordx777@gmail.com>
2024-09-18 06:33:53 +0700

    add DOM and declare initial variable

--- index.js
+++ index.js
@@ -6,3 +6,15 @@
 //calculate the result on =, or calculate change a[]b into a then insert b 
 //  if other operation is selected instead
 
+//if number clicked, put number on monitor,
+//if other button is clicked, put 0 on the monitor
+const monitor = document.querySelector(".lcd")
+const numberPad = document.querySelectorAll(".number")
+const operatorPad = document.querySelectorAll(".operator")
+
+var numClick=0
+
+var text = "0";
+var first = 0;
+var second = 0;
+
