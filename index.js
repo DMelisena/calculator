0eb010d3a27974009365d32e2a4420a11ae80193
DMelisena <demonlordx777@gmail.com>
2024-09-18 04:47:37 +0700

    Adding what to do on javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+//TODO:
+//A way to show numbers on LCD. first insert a
+//a variable that states the operator (add/sub/times/divide/remainder)
+//after the type of calculation set, delete the letter on monitor,insert b
+//should the type of calculation be shown in the monitor?
+//calculate the result on =, or calculate change a[]b into a then insert b 
+//  if other operation is selected instead
+
